dc12548933a02705622d7ea0bf0eea9da5e05775
inemesitaffia <devveridian@gmail.com>
2017-01-04 20:14:02 +0100

    added wintellect powercollections

--- 12or1x/gennumbers.cs
+++ 12or1x/gennumbers.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StaticRandom;
+using Verhoeff;
+using Wintellect.PowerCollections;
+
 
 namespace _12or1x
 {
     public class gennumbers
     {
+        public static string genCheckedDigits()
+        {
+            var numbers = gen11Digits();
+            var generatedVerhoeff = Verhoeff.VerhoeffChecker.generateVerhoeff(numbers);
+            Console.WriteLine(numbers);
+            return numbers + generatedVerhoeff;
+
+        }
+        public static string gen11Digits()
+        {
+
+            var d1 = StaticRandom.StaticRandom.Next(0, 9).ToString();
+            var d2 = StaticRandom.StaticRandom.Next(0, 9);
+            var d3 = StaticRandom.StaticRandom.Next(0, 9);
+            var d4 = StaticRandom.StaticRandom.Next(0, 9);
+            var d5 = StaticRandom.StaticRandom.Next(0, 9);
+            var d6 = StaticRandom.StaticRandom.Next(0, 9);
+            var d7 = StaticRandom.StaticRandom.Next(0, 9);
+            var d8 = StaticRandom.StaticRandom.Next(0, 9);
+            var d9 = StaticRandom.StaticRandom.Next(0, 9);
+            var d10 = StaticRandom.StaticRandom.Next(0, 9);
+            var d11 = StaticRandom.StaticRandom.Next(0, 9);
+
+            return d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11;
+        }
+        static void Main(string[] args)
+        {
+
+        }
     }
+
+
 }
