c4e1fc8d86d9ac9ad2d202c0d4a2e34fdf854aa1
inemesitaffia <devveridian@gmail.com>
2017-01-04 20:09:41 +0100

    added scaffolding to number gen

--- 12or1x/Program.cs
+++ 12or1x/Program.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace _12or1x
 {
-    static class Program
+    public static class Program
     {
         /// <summary>
         /// The main entry point for the application.
